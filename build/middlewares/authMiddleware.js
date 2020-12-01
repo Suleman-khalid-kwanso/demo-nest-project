7cb8e255d15d180eb380f400f8a1c7c73941c6f6
suelman-khalid <suelman-khalid@kwanso.com>
2020-12-01 14:45:09 +0500

    clear code

--- build/middlewares/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authMiddleware = void 0;
-var authMiddleware = function (req, res, next) {
-    console.log(res);
-    next();
-};
-exports.authMiddleware = authMiddleware;
-//# sourceMappingURL=authMiddleware.js.map

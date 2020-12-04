373a09374bf4775457bbc2cf49775680fde8dc61
suelman-khalid <suelman-khalid@kwanso.com>
2020-12-04 19:42:56 +0500

    added graph ql and resovle error

--- src/photo/photo.module.ts
+++ src/photo/photo.module.ts
@@ -3,10 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from './photo.entity';
 import { PhotoService } from './photo.service';
 import { PhotoController } from './photo.controller';
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
+import { PhotoQlDto } from './dto/photoQlDto';
+import { PhotoResolver } from './photo.resolver';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Photo])],
-  providers: [PhotoService],
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([Photo])],
+      resolvers: [{ DTOClass: PhotoQlDto, EntityClass: Photo }],
+    }),
+  ],
+  providers: [PhotoService, PhotoResolver],
   controllers: [PhotoController],
 })
 export class PhotoModule {}
